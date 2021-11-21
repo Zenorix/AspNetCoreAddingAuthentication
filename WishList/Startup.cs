516087003bc34ad72eb7d854c86825eeda994f1b
Zenorix <4050652+Zenorix@users.noreply.github.com>
2021-11-22 00:51:41 +0200

    add authentication to application

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList
 {
@@ -14,10 +17,11 @@ namespace WishList
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -28,7 +32,9 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
     }
 }
